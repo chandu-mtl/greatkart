84728966ddfdc2141c2296721e734972ca4773d1
Chandu <chandu_mtl@yahoo.com>
2025-10-07 16:30:45 +0530

    11 Completed

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -124,7 +124,8 @@ STATICFILES_DIRS = [
     'greatkart/static',
 ]
 
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
