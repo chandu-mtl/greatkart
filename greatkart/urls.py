995500a028ba15bf2333f77a00bb5ca508c00f5d
Chandu <chandu_mtl@yahoo.com>
2025-10-08 11:44:52 +0530

    17 Completed

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('store/', include('store.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
